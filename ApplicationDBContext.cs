2557f7396fa2e76f1c44290c467a2baa04ef914f
themudit-18 <bhattmudit213@gmail.com>
2025-02-04 08:34:29 +0530

    Add project files.

---
+++ ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LinQ_DB.Employee;
+
+namespace LinQ_DB
+{
+    //DbContext class – it is responsible to communicate with database
+    //DbSet - Represents the collection of all entities in the context, or that can be queried from the database, of a given type.
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-RFJH7F5C\SQLEXPRESS;Initial Catalog=LinQ_DB; Trusted_Connection=true;TrustServerCertificate=True;");//change your connection as required
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using LinQ_DB;
+
+var context = new ApplicationDBContext();
+//For example, Display FirstName of all employees.
+var q1 = context.Employee.Select(x => x.FirstName);
+foreach (var employee in q1)
+{
+    Console.WriteLine("\n {0}", employee);
+}
